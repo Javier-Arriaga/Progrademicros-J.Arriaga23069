ed0fd688090f98fa8f1af6ca3132ba36ea918e80
Javier-Arriaga <arr23069@uvg.edu.gt>
2025-02-04 12:53:44 -0600

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,16 @@
+;***********************************************************
+;Universidad del Valle de Guatemala
+;IE023: Programacion de microcontroladores
+;
+; Created: 4/02/2025 12:21:02
+; Author : Javier Arriaga
+;Proyecto: Lab1 Javier Arriaga.asm
+;***********************************
+
+
+start:
+LDI r16
+ADD r16
+sub r16
+
+    
